0f03c9c7e21b676cb7841f812128f97700fbecaf
Lakr <git@qaq.wiki>
2026-05-02 01:08:59 +0900

    Annotate custom display item nullable fields

--- Sources/LookinCore/LookinCustomDisplayItemInfo.h
+++ Sources/LookinCore/LookinCustomDisplayItemInfo.h
@@ -8,13 +8,15 @@
 
 #import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface LookinCustomDisplayItemInfo : NSObject <NSSecureCoding, NSCopying>
 
 /// 该属性可能有值（CGRect）也可能是 nil（nil 时则表示无图像）
-@property(nonatomic, strong) NSValue *frameInWindow;
-@property(nonatomic, copy) NSString *title;
-@property(nonatomic, copy) NSString *subtitle;
-@property(nonatomic, copy) NSString *danceuiSource;
+@property(nonatomic, strong, nullable) NSValue *frameInWindow;
+@property(nonatomic, copy, nullable) NSString *title;
+@property(nonatomic, copy, nullable) NSString *subtitle;
+@property(nonatomic, copy, nullable) NSString *danceuiSource;
 
 /// YES if this display item represents a SwiftUI virtual node produced by the
 /// activation-gated SwiftUI extension. Hosts use this to render an accent
@@ -28,5 +30,7 @@
 @property(nonatomic, copy, nullable) NSString *swiftUIDisplayItemID;
 
 @end
+
+NS_ASSUME_NONNULL_END
 
 #endif /* SHOULD_COMPILE_LOOKIN_SERVER */
